5d0e155fa6171289e93b5c2603fe85e5f2931c5c
fernanluis <luisfer439@hotmail.com>
2020-11-07 19:49:41 -0300

    Testing the App

--- app/controllers/travel_controller.rb
+++ app/controllers/travel_controller.rb
@@ -1,18 +1,28 @@
 class TravelController < ApplicationController
+
   def index
   end
 
   def search
-   unless countries
-     countries = find_country(params[:country])
-     flash[:alert] = 'Country not found'
-     return render action: :index
-   end
+    countries = find_country(params[:country])
 
-   @countries = countries.first
+    unless countries
+       flash[:alert] = 'Country not found'
+       return render action: :index
+     end
+
+   @country = countries.first
    @weather = find_weather(@country['capital'], @country['alpha2Code'])
   end
 
+  def find_weather(city, country_code)
+    query = URI.encode("#{city},#{country_code}")
+
+    request_api(
+      "https://community-open-weather-map.p.rapidapi.com/forecast?q=#{query}"
+    )
+  end
+    
   private
   def request_api(url)
     response = Excon.get( # solicitud get a la url especificada y envía mi clave RapidAPI (que toma
@@ -31,14 +41,6 @@ class TravelController < ApplicationController
   def find_country(name)
     request_api(
       "https://restcountries-v1.p.rapidapi.com/name/#{URI.encode(name)}"
-    )
-  end
-
-  def find_weather(city, country_code)
-    query = URI.encode("#{city},#{country_code}")
-
-    request_api(
-      "https://community-open-weather-map.p.rapidapi.com/forecast?q=#{query}"
     )
   end
 
