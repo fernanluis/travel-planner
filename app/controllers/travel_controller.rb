74085592ec2ab2c62b547fb9c48eaaf7408a10e2
fernanluis <luisfer439@hotmail.com>
2020-11-07 18:57:44 -0300

    Adding Weather and Putting it all Together

--- app/controllers/travel_controller.rb
+++ app/controllers/travel_controller.rb
@@ -3,11 +3,14 @@ class TravelController < ApplicationController
   end
 
   def search
-   countries = find_country(params[:country])
    unless countries
+     countries = find_country(params[:country])
      flash[:alert] = 'Country not found'
      return render action: :index
    end
+
+   @countries = countries.first
+   @weather = find_weather(@country['capital'], @country['alpha2Code'])
   end
 
   private
@@ -24,10 +27,18 @@ class TravelController < ApplicationController
     return nil if response.status != 200
     JSON.parse(response.body)
   end
-# el método find_country usa API REST Countries para obtener datos sobre un pais  
+    # el método find_country usa API REST Countries para obtener datos sobre un pais
   def find_country(name)
     request_api(
       "https://restcountries-v1.p.rapidapi.com/name/#{URI.encode(name)}"
+    )
+  end
+
+  def find_weather(city, country_code)
+    query = URI.encode("#{city},#{country_code}")
+
+    request_api(
+      "https://community-open-weather-map.p.rapidapi.com/forecast?q=#{query}"
     )
   end
 
